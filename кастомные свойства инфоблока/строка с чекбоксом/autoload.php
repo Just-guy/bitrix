aaf18b8f0faceace2afa3686ed960a9d4c6a10e1
Just-guy <62421873+Just-guy@users.noreply.github.com>
2023-03-17 15:29:09 +0300

    Update autoload.php

--- кастомные свойства инфоблока/строка с чекбоксом/autoload.php
+++ кастомные свойства инфоблока/строка с чекбоксом/autoload.php
@@ -2,7 +2,5 @@
 use Bitrix\Main\Loader;
 
 Loader::registerAutoLoadClasses(null, [
-	'customClass\likeSystem' => APP_CLASS_FOLDER . '/customClass/likeSystemClass.php',
-	'customClass\dataRecaptchaV3' => APP_CLASS_FOLDER . '/customClass/dataRecaptchaV3.php',
 	'lib\customClass\usertype\CUserStringWithCheckbox' => APP_CLASS_FOLDER . 'customClass/usertype/CUserStringWithCheckbox.php',
 ]);
