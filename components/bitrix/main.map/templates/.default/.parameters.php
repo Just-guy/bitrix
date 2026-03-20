8844dad9fc07ffb5390edd8c337db612e0970fb9
Андрей Белый <Andrej.Belyj@itb-dev.ru>
2026-03-20 08:01:33 +0300

    Подключил модуль iblock в параметрах

--- components/bitrix/main.map/templates/.default/.parameters.php
+++ components/bitrix/main.map/templates/.default/.parameters.php
@@ -1,6 +1,8 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
+CModule::IncludeModule("iblock");
+
 $arIBlockType = CIBlockParameters::GetIBlockTypes();
 $arIBlock = array();
 $iblockFilter = [
